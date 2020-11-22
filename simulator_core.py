83c54f3d87b0737f6d8859ca57dd1b03bfe82cd7
Adrian Glapiński <adrian.glapinski@rsqtechnologies.com>
2020-11-22 19:58:37 +0100

    Create initial implementation of next_day method

--- simulator_core.py
+++ simulator_core.py
@@ -1,12 +1,13 @@
 # Należy przygotować simulator_core, której
 # – konstruktor powinien mieć w swych parametrach konfigurację podzbioru
-#   kampanii/partnerów (podzbiór wartości partner_id) objętych daną symulacją i konfigurację podzbióru
+#   kampanii/partnerów (podzbiór wartości partner_id) objętych daną symulacją i konfigurację podzbióru,
 #   kampanii/partnerów (podzbiór wartości partner_id) objętych odczytem danych jako listę partners_to_read_data_from
 #   instancji obiektów klasy PLO_partner_data_reader,
 # – konstruktor powinien tworzyć listę instancji klasy per_partner_simulator – dla każdej kampanii (partnera)
 #   objętej daną symulacją,
 # – metoda next_day() powinna skutkować odczytaniem z partners_to_read_data_from danych
 #   o kolejnym dniu równolegle symulowanych kampanii.
+from datetime import timedelta
 from functools import reduce
 
 from sortedcontainers import SortedSet
@@ -22,12 +23,18 @@ class SimulatorCore:
                                      self.simulation_partner_ids]
         self.per_partner_simulators = [PerPartnerSimulator(partner_id) for partner_id in self.simulation_partner_ids]
         self.dates = SortedSet(reduce(list.__add__, [data_reader.dates for data_reader in self.partner_data_readers]))
+        self.current_date = self.dates[0] - timedelta(days=1)
 
     def next_day(self):
-        for date in self.dates:
-            print(date)
+        next_day_data = {}
+        for data_reader in self.partner_data_readers:
+            partner_id = data_reader.partner_id
+            next_day_data[partner_id] = data_reader.__next__()
+        self.current_date += timedelta(days=1)
+        return next_day_data
 
 
 if __name__ == '__main__':
     simulator_core = SimulatorCore(['743B1EE3A39E06D855A72B3B66D501D0', '2AAA4123BE41F050F159BD574800464F'])
-    simulator_core.next_day()
+    data = simulator_core.next_day()
+    print(data)
