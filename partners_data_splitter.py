696444303aaacc6f1f775abd0823cfce64380556
Adrian Glapiński <adrian.glapinski@rsqtechnologies.com>
2021-01-02 15:59:56 +0100

    Add PartnersDataSplitterConfig

--- partners_data_splitter.py
+++ partners_data_splitter.py
@@ -1,19 +1,24 @@
 import pickle
 
 import pandas as pd
+import yaml
 
 
 class PartnersDataSplitter:
-    def __init__(self, click_cost_ratio, filepath, nrows=None, outdir='out'):
+    def __init__(self, config):
+        self.filepath = config['data_splitter']['dataset_input_filepath']
+        self.outdir = config['data_splitter']['partners_data_output_directory']
+        self.nrows = config['data_splitter']['nrows']
+        self.click_cost_ratio = config['data_splitter']['click_cost_ratio']
         self.raw_data_frame = None
         self.splitted_data_frames = None
         self.partners_avg_click_costs = None
-        self.click_cost_ratio = click_cost_ratio
-        self.filepath = filepath
-        self.nrows = nrows
-        self.outdir = outdir
 
-    def load_raw_data(self):
+    def split_and_save_data(self):
+        self.__group_data_by_partners_and_dates()
+        self.__save_groups_to_pickle()
+
+    def __load_raw_data(self):
         header_info = "sale,sales_amount_in_euro,time_delay_for_conversion,click_timestamp,nb_clicks_1week," \
                       "product_price,product_age_group,device_type,audience_id,product_gender,product_brand," \
                       "product_category_1,product_category_2,product_category_3,product_category_4," \
@@ -29,8 +34,8 @@ class PartnersDataSplitter:
         self.raw_data_frame = pd.read_csv(self.filepath, delimiter='\t', header=None,
                                           names=header_info.split(','), dtype=dtypes, nrows=self.nrows)
 
-    def group_data_by_partners_and_dates(self):
-        self.load_raw_data()
+    def __group_data_by_partners_and_dates(self):
+        self.__load_raw_data()
         df = self.raw_data_frame
 
         df['sales_amount_in_euro'] = df['sales_amount_in_euro'].replace(-1.0, pd.NA)
@@ -42,20 +47,21 @@ class PartnersDataSplitter:
         self.splitted_data_frames = {partner_id: partner_data_frame.groupby('date') for partner_id, partner_data_frame
                                      in partner_id_groups}
 
-    def save_groups_to_pickle(self):
-        for partner_id, partner_id_date_df_groups in self.splitted_data_frames.items():
-            with open(f'{self.outdir}/{partner_id}.pickle', "wb") as file:
-                pair = (partner_id_date_df_groups, self.partners_avg_click_costs[partner_id])
-                pickle.dump(pair, file)
-
     def __calculate_partners_avg_click_costs(self, partner_id_groups):
         total_partners_clicks_count = partner_id_groups.size()
         total_partners_sales = partner_id_groups['sales_amount_in_euro'].sum().apply(
             lambda total_sales: total_sales * self.click_cost_ratio)
         self.partners_avg_click_costs = total_partners_sales / total_partners_clicks_count
 
+    def __save_groups_to_pickle(self):
+        for partner_id, partner_id_date_df_groups in self.splitted_data_frames.items():
+            with open(f'{self.outdir}/{partner_id}.pickle', "wb") as file:
+                pair = (partner_id_date_df_groups, self.partners_avg_click_costs[partner_id])
+                pickle.dump(pair, file)
+
 
 if __name__ == '__main__':
-    data_splitter = PartnersDataSplitter(click_cost_ratio=0.12, filepath='criteo/CriteoSearchData')
-    data_splitter.group_data_by_partners_and_dates()
-    data_splitter.save_groups_to_pickle()
+    with open('config.yaml', 'r') as config_file:
+        yaml_config = yaml.safe_load(config_file)
+    data_splitter = PartnersDataSplitter(yaml_config)
+    data_splitter.split_and_save_data()
